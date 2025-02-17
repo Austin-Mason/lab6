40fa6c698718530511bac23da710b6e6cbca9225
Austin-Mason <masonaustin240@gmail.com>
2025-02-17 17:37:48 -0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        double celsius, fahrenheit;
+
+        while (true) {
+            System.out.print("Enter temp in Celsius: ");
+            if (scanner.hasNextDouble()) {
+                celsius = scanner.nextDouble();
+                fahrenheit = (celsius * 9 / 5) + 32;
+                System.out.printf("The temp in fahrenheit: %.2f°F\n", fahrenheit);
+                break;
+            } else {
+                System.out.println("Invalid input.");
+
+            }
+        }
     }
 }
--- src/question2.java
+++ src/question2.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class question2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double gallons, mpg, pricePerGallon;
+
+
+        do {
+            System.out.print("Enter the number of gallons of gas in the tank: ");
+            while (!scanner.hasNextDouble()) {
+                System.out.println("Invalid input.");
+                scanner.next();
+            }
+            gallons = scanner.nextDouble();
+        } while (gallons <= 0);
+
+
+        do {
+            System.out.print("Enter the fuel efficiency in miles per gallon: ");
+            while (!scanner.hasNextDouble()) {
+                System.out.println("Invalid input.");
+                scanner.next();
+            }
+            mpg = scanner.nextDouble();
+        } while (mpg <= 0);
+
+
+        do {
+            System.out.print("Enter the price of gas per gallon: ");
+            while (!scanner.hasNextDouble()) {
+                System.out.println("Invalid input.");
+                scanner.next();
+            }
+            pricePerGallon = scanner.nextDouble();
+        } while (pricePerGallon <= 0);
+
+
+        double costPer100Miles = (100 / mpg) * pricePerGallon;
+
+
+        double maxDistance = gallons * mpg;
+
+        // Display results
+        System.out.printf("The cost to drive 100 miles: $%.2f\n", costPer100Miles);
+        System.out.printf("The car can go about %.2f miles with a full tank.\n", maxDistance);
+    }
 }
